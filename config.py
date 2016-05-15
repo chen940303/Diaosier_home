dae9f2ba2be9f8a2aeacab7adac5d54335743b55
chen940303 <447325059@qq.com>
2016-05-16 03:27:19 +0800

    product config 2

--- config.py
+++ config.py
@@ -51,7 +51,7 @@ class ProductionConfig(Config):
         secure=None
         if getattr(cls,'MAIL_USE_TLS',None) is not None:
             credentials=(cls.MAIL_USE_TLS,None)
-            if getarrt(cls, 'MAIL_USE_TLS',None):
+            if getattr(cls, 'MAIL_USE_TLS',None):
                 secure()
         mali_handler=SMTPHandler(mailhost=(cls.MAIL_SERVER, cls.MAIL_PORT), fromaddr=cls.FLASKY_MAIL_SENDER,toaddrs=[cls.FLASKY_ADMIN],subject=cls.FLASKY_MAIL_SUBJECT_PREFIX + ' Application Error',credentials=credentials,secure=secure)
         mail_handler.setLevel(logging.ERROR)
